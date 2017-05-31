53a570335fe0c6786b621a01ec504c31525f685f
Henning <henning-maier@web.de>
2017-05-31 23:50:50 +0200

    Added basic Angular-CLI + Components Menu, Task-list, task-element

---
+++ Frontend/src/app/menu/task-list/task-element/task-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-element',
+  templateUrl: './task-element.component.html',
+  styleUrls: ['./task-element.component.css']
+})
+export class TaskElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
