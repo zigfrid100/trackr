bad241fe8a0d9785c5724e79dfb325ef589af020
Leon <studium-guenther@t-online.de>
2017-06-01 16:21:01 +0200

    edit user schema

--- Backend/app/models/user.js
+++ Backend/app/models/user.js
@@ -1,3 +1,28 @@
 /**
  * Created by Leon on 01.06.2017.
  */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Defining schema for the model Task
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// Registering a model Task with given mongoose schema
+module.exports = mongoose.model('User', UserSchema);
