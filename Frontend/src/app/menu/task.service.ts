5b031b9244915b027daa5a6d6e49bccada0ed3b9
Henning <henning-maier@web.de>
2017-06-08 16:08:53 +0200

    Added Task Service

--- Frontend/src/app/menu/task.service.ts
+++ Frontend/src/app/menu/task.service.ts
@@ -11,7 +11,9 @@ export class TaskService {
   public online: Observable<boolean>;
   public isonline: boolean;
 
-  constructor() {
+  public projects: any[];
+
+  constructor(private http: Http) {
     this.online = Observable.merge(
       Observable.of(navigator.onLine),
       Observable.fromEvent(window, 'online').map(() => true),
@@ -19,5 +21,48 @@ export class TaskService {
     );
     this.online.subscribe(status => this.isonline = status);
   }
+
+  getProjects() {
+    console.log('get Jokes');
+    this.http.get('http://51.254.103.142:3000/project')
+      .map(response => response.json()).subscribe(
+      (responseItems: any[]) => {
+        responseItems.forEach((responseItem: any) => {
+          responseItem.statusVal = 'active';
+          this.projects.push(responseItem);
+
+        });
+      },
+      (err: any) => {
+        if(err.status == 0){
+          alert('Server down')
+        }else {
+          alert('Error: ' + err.json().message);
+          console.log('Error: ' + err.json().message);
+        }
+      }
+    );
+  }
+
+  getProject(id) {
+    console.log('get Jokes:id');
+    this.http.get('http://51.254.103.142:3000/project' + id)
+      .map(response => response.json()).subscribe(
+      (responseItem: any) => {
+        responseItem.statusVal = 'active';
+        this.projects.push(responseItem);
+      },
+      (err: any) => {
+        if(err.status == 0){
+          alert('Server down')
+        }else {
+          alert('Error: ' + err.json().message);
+          console.log('Error: ' + err.json().message);
+        }
+      }
+    );
+  }
+
+
 
 }
