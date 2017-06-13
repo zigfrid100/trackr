e8200927772b48f87cadcd5d304880bc5d6220bf
Henning <henning-maier@web.de>
2017-06-13 17:58:00 +0200

    Display time spent on Interval after pause is clicked

--- Frontend/src/app/menu/task-list/task-element/task-element.component.ts
+++ Frontend/src/app/menu/task-list/task-element/task-element.component.ts
@@ -9,23 +9,24 @@ import { TaskService } from '../../../task.service';
 export class TaskElementComponent implements OnInit {
 
   @Input() task: any;
-  constructor(private taskService: TaskService) {
-
-  }
-
   show: boolean = false;
   startbtn: boolean = true;
   pausebtn: boolean = false;
-
+  starttime: any;
+  endtime: any;
+  constructor(private taskService: TaskService) {}
   ngOnInit() {
   }
 
   start() {
     this.taskService.startTask(this.task._id, 'something');
+    this.starttime = Date.now();
+    this.endtime = Date.now();
   }
 
   pause() {
     this.taskService.pauseTask(this.task._id);
+    this.endtime = Date.now();
   }
 
   stop() {
